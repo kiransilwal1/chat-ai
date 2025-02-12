21fde407d3fcaddb795e6cf14f26a4d5d5bbc332
kiransilwal1 <kiran.silwal1@gmail.com>
2025-02-12 21:33:40 +0545

    Deleting chat model and migrating to chat explorer model

--- features/chat/chat_model/chat_repository_impl.py
+++
@@ -1,6 +0,0 @@
-from features.chat.chat_model.chat_repositroy_interface import IChatRepository
-
-
-# class ChatModelRepository(IChatRepository):
-#     def __init__(self, llama_model: Llama):
-#         self._llama_mode = llama_model
--- features/chat/chat_model/chat_repositroy_interface.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IChatRepository(ABC):
-    @abstractmethod
-    async def chat(self, query_string: str) -> str: ...
